30021687f8efc324e6d04718173153afd17c5f40
Sufyan-Virk <sufyanvirk381@gmail.com>
2026-07-08 08:08:12 -0700

    Product crud is added.

---
+++ product/dto/create-product.dto.ts
@@ -0,0 +1,11 @@
+export class CreateProductDto {
+  readonly name: string;
+  readonly description: string;
+  readonly price: number;
+}
+
+export class UpdateProductDto {
+  readonly name?: string;
+  readonly description?: string;
+  readonly price?: number;
+}
